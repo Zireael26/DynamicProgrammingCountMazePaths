bb92b412ae6b46b724ddea3fd857d53d25b37231
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2018-09-18 11:50:26 +0530

    Added memoized and tabulated approaches

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,22 @@ public class Main {
 //
 //        System.out.println("Time taken: " + (endPro - startPro) + "ms");
 //        System.out.println("Time taken: " + (endReact - startReact) + "ms");
-        long start = System.currentTimeMillis();
-        System.out.println(countMazePath(0, 0, 15, 15));
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken: " + (end - start) + "ms");
+
+//        long start = System.currentTimeMillis();
+//        System.out.println(countMazePath(0, 0, 15, 15));
+//        long end = System.currentTimeMillis();
+//        System.out.println("Time taken: " + (end - start) + "ms");
+
+        int n = 15;
+        long startMem = System.currentTimeMillis();
+        System.out.println(countMazePathMemoized(0, 0, n, n, new int[n + 1][n + 1]));
+        long endMem = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endMem - startMem) + "ms");
+
+        long startTab = System.currentTimeMillis();
+        System.out.println(countMazePathTabulated(n, n));
+        long endTab = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTab - startTab) + "ms");
     }
 
     public static void printMazePathsProactive(int srcX, int srcY, int destX, int destY, String pathSoFar) {
@@ -70,4 +82,49 @@ public class Main {
         return countSrcToDest;
     }
 
+    public static int countMazePathMemoized(int srcX, int srcY, int destX, int destY, int[][] memoizedArr) {
+        if (srcX == destX && srcY == destY) { // if already on destination square, there is only 1 path (not moving)
+            return 1;
+        }
+
+        // if outside the board bounds, no path
+        if (srcX > destX || srcY > destY) {
+            return 0;
+        }
+        // if we have already solved the problem, why calculate? Simply take it from the 2D Array
+        if (memoizedArr[srcX][srcY] != 0) {
+            return memoizedArr[srcX][srcY];
+        }
+
+        int countSrcToDest = 0;
+        // horizontal call
+        int countInterHorizToDest = countMazePathMemoized(srcX, srcY + 1, destX, destY, memoizedArr);
+        // vertical call
+        int countInterVertToDest = countMazePathMemoized(srcX + 1, srcY, destX, destY, memoizedArr);
+        countSrcToDest = countInterHorizToDest + countInterVertToDest;
+
+        // store in memoized array before returning
+        memoizedArr[srcX][srcY] = countSrcToDest;
+        return countSrcToDest;
+    }
+
+    public static int countMazePathTabulated(int destX, int destY) {
+        int[][] f = new int[destX + 1][destY + 1];
+
+        for (int x = destX; x >= 0; x--) { // we move from last cell (known value) towards the one we want (first cell)
+            for (int y = destY; y >= 0; y--) {
+                if (x == destX && y == destY) {  // last cell
+                    f[x][y] = 1;
+                } else if (x == destX) {         // last row, except last cell
+                    f[x][y] += f[x][y+1];
+                } else if (y == destY) {         // last column, except last cell
+                    f[x][y] += f[x+1][y];
+                } else {                         // any other row/column
+                    f[x][y] += f[x + 1][y] + f[x][y + 1];
+                }
+            }
+        }
+
+        return f[0][0];
+    }
 }
